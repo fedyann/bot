9e29ea5ee8c8e2196eb2725640bd1899fb3ac647
fedyann <annfedotova6@yandex.ru>
2023-04-04 23:11:20 +0300

    start + help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 from telegram.ext import Application, MessageHandler, filters
+from telegram.ext import CommandHandler
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
@@ -9,17 +10,38 @@ logger = logging.getLogger(__name__)
 
 
 async def echo(update, context):
-    await update.message.reply_text(update.message.text)
+    await update.message.reply_text('Чтобы начать, нажми /start')
+
+async def start(update, context):
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Привет, {user.mention_html()}! Я помогу тебе сделать твой собственный мем. Сначала выбери шаблон!",
+    )
 
 
+async def help_command(update, context):
+    """Отправляет сообщение когда получена команда /help"""
+    await update.message.reply_text("Я пока не умею помогать...")
+
+
+
+
+# Зарегистрируем их в приложении перед
+# регистрацией обработчика текстовых сообщений.
+# Первым параметром конструктора CommandHandler я
+# вляется название команды.
 def main():
     application = Application.builder().token('5822667731:AAF2BwfBkHzk9Di3SoydIF4GMBhKamn8wfA').build()
 
-    text_handler = MessageHandler(filters.TEXT, echo)
+    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
 
     application.add_handler(text_handler)
 
     application.run_polling()
+
 
 
 if __name__ == '__main__':
